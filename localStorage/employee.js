cac48fc8e2dad186f1a73522f5dfc080f8f2c2ab
Girish <rw3.girish.gk@gmail.com>
2025-01-30 09:59:40 +0530

    read & delete

--- localStorage/employee.js
+++ localStorage/employee.js
@@ -19,6 +19,40 @@ addEmpFrom.addEventListener("submit", (e) => {
 
     employees.push(newEmp);
     localStorage.setItem("employees", JSON.stringify(employees));
-    // console.log(newEmp);
+   
     addEmpFrom.reset();
-})
+    loadData()
+});
+
+
+function loadData() {
+    let allData = JSON.parse(localStorage.getItem('employees'))
+    // console.log(allData);
+
+    let result = "";
+    allData.forEach((record, index) => {
+        let row = `<tr>
+        <td>${index + 1}</td>
+        <td>${record.name}</td>
+        <td>${record.email}</td>
+        <td>${record.mobileNo}</td>
+        <td>${record.role}</td>
+        <td><button onclick="deleteEmployee(${index})">Delete</button> || <button onclick="editEmployee(${index})">Edit</button></td>
+        </tr>`
+        result = result + row;
+    });
+
+    document.querySelector('#viewData').innerHTML = result
+}
+
+loadData()
+
+
+function deleteEmployee(index) {
+    let allData = JSON.parse(localStorage.getItem('employees'))
+    // console.log(allData[index])
+    allData.splice(index, 1);
+
+    localStorage.setItem("employees", JSON.stringify(allData));
+    loadData();
+}
